65236b9ab69114b32970e6f40ffb3866d6dbc519
matsu1005 <forte4262@gmail.com>
2019-10-11 20:35:26 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,48 @@
 $(function(){
-  $(".submit-btn").on('submit',function(e){
+  function buildHTML(message) {
+    var content = message.text ? `${ message.text }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html =`<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                      ${message.user_name}
+                      </p>
+                      <p class="message__upper-info__date">
+                      ${message.date}
+                      </p>
+                    </div>
+                    <p class="message__text">
+                    ${content}
+                    </p>
+                  ${img}
+                </div>`
+  return html;
+  }
+
+  $(".new_message").on('submit', function(e){
     e.preventDefault();
-    console.log("test")
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form-box__text').val('');
+      $('.form-box__image__file').val('');
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);　//ここで解除している
+    })
   })
-})
+});
