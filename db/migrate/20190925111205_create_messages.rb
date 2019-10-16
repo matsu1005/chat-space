c28571047dedc3d59ccabf92edc08745e04b1649
matsu1005 <forte4262@gmail.com>
2019-10-16 18:33:25 +0900

    Update 20190925111205_create_messages.rb

--- db/migrate/20190925111205_create_messages.rb
+++ db/migrate/20190925111205_create_messages.rb
@@ -1,5 +1,5 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
-  def up
+  def change
     drop_table :messages
     create_table :messages do |t|
       t.string     :text
@@ -8,7 +8,5 @@ class CreateMessages < ActiveRecord::Migration[5.0]
       t.references :group, foreign_key: true
       t.timestamps
     end
-  end
-  def down
   end
 end
