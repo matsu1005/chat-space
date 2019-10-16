3a075c99571d839fd7f6a97d89239713295f9a13
matsu1005 <forte4262@gmail.com>
2019-10-16 20:00:52 +0900

    Update 20190925111205_create_messages.rb

--- db/migrate/20190925111205_create_messages.rb
+++ db/migrate/20190925111205_create_messages.rb
@@ -1,6 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
-    drop_table :messages
+    # drop_table :messages
     create_table :messages do |t|
       t.string     :text
       t.string     :image
