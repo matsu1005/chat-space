8519232f9491f0c844b8d2880e9b4c88b3fcdc45
matsu1005 <forte4262@gmail.com>
2019-10-17 20:42:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-after 'desplay:publishing', 'deploy:restart'
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do 
   task :restart do 
     invoke 'unicorn:restart'
