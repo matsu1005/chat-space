d51bb8994e084428d4d33d78f27f1e147d5d2b7e
matsu1005 <forte4262@gmail.com>
2019-10-17 20:45:35 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/chat-space191016.pem']
+                  keys: ['~/.ssh/chatspace191016.pem']
 
 set :unicorn_pid, -> {"#{shared_path}/tmp/pids/unicorn.pid"}
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb"}
